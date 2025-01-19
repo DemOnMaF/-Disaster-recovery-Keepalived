17f67101597ac9dcde0fa94c9ab3c431d637be20
DemOnMaF <demon.maf@gmail.com>
2025-01-19 22:32:59 +0300

    Update check_nginx.sh

--- check_nginx.sh
+++ check_nginx.sh
@@ -2,5 +2,5 @@
 if [[ $(netstat -tuln | grep LISTEN | grep :80) ]] && [[ -f /var/www/html/index.nginx-debian.html ]]; then
         exit 0
 else
-        sudo systamctl stop keepalived
+        sudo systemctl stop keepalived
 fi
